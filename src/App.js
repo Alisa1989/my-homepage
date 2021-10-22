7baa674f7679464d7c736c6cd79ca17c5a91de53
alexandre steinhauslin <a.steinhauslin@gmail.com>
2021-10-21 18:16:34 -0700

    added routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import "./App.css";
 import React from "react";
 import ToDo from './components/ToDoSection/ToDo'
 import NewsList from './components/NewsSection/NewsList'
+import {Link, Route} from 'react-router-dom';
 
 //Adding local state to the class
 function App () {
     return(
-      <div className="App" style={{backgroundImage: `url("https://images2.alphacoders.com/166/thumb-1920-166600.jpg")`, backgroundSize: 'cover', height: '100vh', backgroundPosition:'center'}}>
+      <div className="App" style={{backgroundImage: `url("https://images2.alphacoders.com/166/thumb-1920-166600.jpg")`, backgroundSize: 'cover', height: '100vh', backgroundPosition:'center', backgroundAttachment:'fixed', height:"100%"}}>
         <div className="header">
           <h2>Main</h2>
-          <NewsList/>
-          <ToDo />
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            {/* <Link to="/News">News</Link> */}
+            <Link to="/ToDo">ToDo</Link>
+          </div>
         </div>
+        <Route exact path="/"  component={NewsList}/>
+        {/* <Route path="/News" component={NewsList}/> */}
+        <Route path="/ToDo" component={ToDo}/>
       </div>
     );
 }
--- src/components/NewsSection/NewsItem.js
+++ src/components/NewsSection/NewsItem.js
@@ -1,9 +1,36 @@
 import React from "react";
+import {
+  Button,
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardBody,
+  CardTitle,
+  CardText,
+  Col,
+} from "reactstrap";
 
 function NewsItem(props) {
-    return(
-        <div>{props.title}</div>
-    )
+  console.log(props);
+  const { news } = props;
+  return (
+    <Col xs="6" md="4" xl="3">
+      <Card>
+        <CardImg top width="100%" src={news.urlToImage} alt="article" />
+        <CardBody>
+          <CardTitle tag="h5">{news.source.name}</CardTitle>
+          {/* <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle> */}
+          <CardText>{news.title}</CardText>
+          <a href={news.url} target="_blank" > 
+            <Button color="info">
+              <small className="text-muted">To Article</small>
+            </Button>
+          </a>
+        </CardBody>
+      </Card>
+    </Col>
+    
+  );
 }
 
-export default NewsItem
+export default NewsItem;
